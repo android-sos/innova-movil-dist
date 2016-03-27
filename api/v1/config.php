afc85f3d0d7640e7897e7e82b5130ecedd5e4370
Samuel Ospina <samuel_carlo77@hotmail.com>
2016-03-27 16:56:39 -0430

    Error de Template

--- api/v1/config.php
+++ api/v1/config.php
@@ -3,15 +3,15 @@
  * Database configuration
  * pgsql, mysql
  */
-/*define('DB_TYPE', 'pgsql');
+define('DB_TYPE', 'pgsql');
 define('DB_USERNAME', 'innova');
 define('DB_PASSWORD', '12345');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'innova_web');*/
+define('DB_NAME', 'innova_web');
 
-define('DB_TYPE', 'pgsql');
+/*define('DB_TYPE', 'pgsql');
 define('DB_USERNAME', 'innova_web');
 define('DB_PASSWORD', '12345');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'innova_web');
+define('DB_NAME', 'innova_web');*/
 ?>
