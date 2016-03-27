544999b7d513150cb9ea9aca73f7122ff5929144
Samuel Ospina <samuel_carlo77@hotmail.com>
2016-03-27 17:11:13 -0430

    api ini

--- api/v1/config.php
+++ api/v1/config.php
@@ -8,7 +8,7 @@ define('DB_USERNAME', 'innova');
 define('DB_PASSWORD', '12345');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'innova_web');
- 
+
 /*define('DB_TYPE', 'pgsql');
 define('DB_USERNAME', 'innova_web');
 define('DB_PASSWORD', '12345');
